701bdc9271ddb5d1a72bd39b6d7a85aec3cab57f
ramadevi1092 <ramadevi.bairuboina@outlook.com>
2025-06-23 20:44:24 +0530

    Update and rename main.py to app.py

---
+++ app.py
@@ -0,0 +1,85 @@
+import os
+from flask import jsonify
+from flask import render_template,  json
+from flask_cors import CORS
+from flask import Flask
+import logging
+
+import os
+import os
+
+import psycopg2
+
+from WorkflowAgents.execute import run_agents
+from static.Telemetry import ReadTelemetry,CountEventStatus
+
+app = Flask(__name__)
+
+
+from socket_io_setup import socketio
+socketio.init_app(app, cors_allowed_origins="*")
+# socketio = SocketIO(app, cors_allowed_origins="*", allow_EIO3=True)
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['model'] = ""
+app.config['mname'] = ""
+app.config['vname'] = ""
+
+# Create a global logger object
+logger = logging.getLogger(__name__)
+
+# Configure the logger to use Stackdriver Logging
+# You can also set the logging level and format if needed
+logging.basicConfig(level=logging.INFO)
+
+
+@app.route("/execute_agents", methods=["GET"])
+async def execute_agents_route():
+    """Asynchronous route to execute agents."""
+    results = await run_agents()  # Correctly await the async function
+    return jsonify(results)
+
+@app.route("/")
+def Landing():  
+  return render_template('index.html')
+
+@app.route("/AgenticWorkflow")
+def AgenticWorkflow():  
+  return render_template('AgenticWorkflow.html')
+
+@app.route("/AgentTelemetry")
+def AgentTelemetry():  
+  return render_template('AgentTelemetry.html')
+
+@app.route("/DataLoader")
+def DataLoader():  
+  return render_template('DataLoader.html')
+
+@app.route("/AgentDashboard")
+def AgentDashboard():  
+  return render_template('AgentDashboard.html')
+
+@app.route("/Contact")
+def Contact():  
+  return render_template('Contact.html')
+
+@app.route("/FetchFromFirestore")
+def FetchFromFirestore():
+    return ReadTelemetry()
+
+
+@app.route("/FetchFromDatabase")
+def FetchFromDatabase():
+    data = []
+    try:
+        event_count = CountEventStatus()
+        return {"EventStatus":event_count}
+    except (Exception, psycopg2.Error) as error:
+        print("Error fetching records from PostgreSQL: ", error)
+        return {"message": "Error fetching records from PostgreSQL: " + error}
+    
+
+if __name__ == '__main__':
+    server_port = os.environ.get('PORT', '8080')
+    app.run(debug=True, port=server_port, host='0.0.0.0')
